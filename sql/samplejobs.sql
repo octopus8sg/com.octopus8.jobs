34e5e5e1065d8ed358614f349d131450e9f6ebe3
octohindol <hindol@octopus8.com>
2021-12-10 23:56:07 +0300

    due date filter in common jobs-1

--- sql/samplejobs.sql
+++ sql/samplejobs.sql
@@ -1,8 +1,18 @@
-insert into civicrm_o8_job (contact_id, title, role_id, location_id, status_id, created_date,created_id)
-select civicrm_contact.id, concat(civicrm_contact.display_name, " JOB"), 1, 1, 1, NOW(),civicrm_contact.id from civicrm_contact where contact_type = "Organization";
-insert into civicrm_o8_job (contact_id, title, role_id, location_id, status_id, created_date,created_id)
-select civicrm_contact.id, concat(civicrm_contact.display_name, " JOB"), 2, 2, 2, NOW(),civicrm_contact.id from civicrm_contact where contact_type = "Organization";
-insert into civicrm_o8_job (contact_id, title, role_id, location_id, status_id, created_date,created_id)
-select civicrm_contact.id, concat(civicrm_contact.display_name, " JOB"), 3, 3, 3, NOW(),civicrm_contact.id from civicrm_contact where contact_type = "Organization";
-insert into civicrm_o8_job (contact_id, title, role_id, location_id, status_id, created_date,created_id)
-select civicrm_contact.id, concat(civicrm_contact.display_name, " JOB"), 1, 2, 3, NOW(),civicrm_contact.id from civicrm_contact where contact_type = "Organization";
+SET @MIN = NOW() - INTERVAL 1 month;
+SET @MAX = NOW();
+
+SET @WOW =  TIMESTAMPADD(SECOND, FLOOR(RAND() * TIMESTAMPDIFF(SECOND, @MIN, @MAX)), @MIN);
+
+delete from civicrm_job where 1 = 1;
+insert into civicrm_o8_job (contact_id, title, role_id, location_id, status_id, created_date,due_date,created_id)
+select civicrm_contact.id, concat(civicrm_contact.display_name, " JOB"), 1, 1, 1, @WOW,(@WOW + INTERVAL 1 MONTH), civicrm_contact.id from civicrm_contact where contact_type = "Organization";
+SET @WOW =  TIMESTAMPADD(SECOND, FLOOR(RAND() * TIMESTAMPDIFF(SECOND, @MIN, @MAX)), @MIN);
+insert into civicrm_o8_job (contact_id, title, role_id, location_id, status_id, created_date,due_date,created_id)
+select civicrm_contact.id, concat(civicrm_contact.display_name, " JOB"), 2, 2, 2, @WOW,(@WOW + INTERVAL 1 MONTH), civicrm_contact.id from civicrm_contact where contact_type = "Organization";
+SET @WOW =  TIMESTAMPADD(SECOND, FLOOR(RAND() * TIMESTAMPDIFF(SECOND, @MIN, @MAX)), @MIN);
+insert into civicrm_o8_job (contact_id, title, role_id, location_id, status_id, created_date,due_date,created_id)
+select civicrm_contact.id, concat(civicrm_contact.display_name, " JOB"), 3, 3, 3, @WOW,(@WOW + INTERVAL 1 MONTH), civicrm_contact.id from civicrm_contact where contact_type = "Organization";
+SET @WOW =  TIMESTAMPADD(SECOND, FLOOR(RAND() * TIMESTAMPDIFF(SECOND, @MIN, @MAX)), @MIN);
+insert into civicrm_o8_job (contact_id, title, role_id, location_id, status_id, created_date,due_date,created_id)
+select civicrm_contact.id, concat(civicrm_contact.display_name, " JOB"), 1, 2, 3, @WOW,(@WOW + INTERVAL 1 MONTH), civicrm_contact.id from civicrm_contact where contact_type = "Organization";
+
